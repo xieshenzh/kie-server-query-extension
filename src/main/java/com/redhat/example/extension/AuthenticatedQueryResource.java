fd445c48f830a61b5e3c8508ae1e68033d943682
xiezhang <xiezhang@redhat.com>
2021-04-07 11:47:07 -0400

    Query with task id

--- src/main/java/com/redhat/example/extension/AuthenticatedQueryResource.java
+++ src/main/java/com/redhat/example/extension/AuthenticatedQueryResource.java
@@ -8,11 +8,12 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.jbpm.services.api.AdvanceRuntimeDataService;
 import org.jbpm.services.api.RuntimeDataService;
+import org.jbpm.services.api.TaskNotFoundException;
+import org.jbpm.services.api.model.UserTaskInstanceDesc;
 import org.jbpm.services.api.model.UserTaskInstanceWithPotOwnerDesc;
 import org.jbpm.services.api.query.QueryService;
 import org.kie.api.runtime.query.QueryContext;
 import org.kie.internal.identity.IdentityProvider;
-import org.kie.internal.task.api.model.TaskEvent;
 import org.kie.server.api.KieServerConstants;
 import org.kie.server.api.model.definition.SearchQueryFilterSpec;
 import org.kie.server.api.model.instance.ProcessInstanceUserTaskWithVariablesList;
@@ -20,17 +21,26 @@ import org.kie.server.remote.rest.common.Header;
 import org.kie.server.remote.rest.common.marker.KieServerEndpoint;
 import org.kie.server.remote.rest.common.util.RestUtils;
 import org.kie.server.services.api.KieServerRegistry;
-import org.kie.server.services.jbpm.ConvertUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
-import java.util.*;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.jbpm.services.api.AdvanceRuntimeDataService.PROCESS_ATTR_INSTANCE_ID;
+import static org.jbpm.services.api.AdvanceRuntimeDataService.TASK_ATTR_NAME;
+import static org.kie.server.api.rest.RestURI.TASK_GET_URI;
+import static org.kie.server.api.rest.RestURI.TASK_INSTANCE_ID;
+import static org.kie.server.remote.rest.common.util.RestUtils.getVariant;
+import static org.kie.server.remote.rest.common.util.RestUtils.notFound;
 
-@Api(value="Process queries")
+
+@Api(value = "Process queries")
 @Path("/server/queries/variables/processes")
 public class AuthenticatedQueryResource {
 
@@ -69,7 +79,7 @@ public class AuthenticatedQueryResource {
         Header conversationIdHeader = RestUtils.buildConversationIdHeader("", context, headers);
         Variant v = RestUtils.getVariant(headers);
         try {
-            final QueryContext queryContext =  new QueryContext(page, pageSize);
+            final QueryContext queryContext = new QueryContext(page, pageSize);
             final AuthQueryUtils authQueryUtils = new AuthQueryUtils(bypassAuthUser, identityProvider, advanceRuntimeDataService, runtimeDataServiceBase);
 
             final List<ProcessInstanceUserTaskDetailsWithVariables> taskDetailsList = authQueryUtils.getUserTaskInstancePotentialOwnerAware
@@ -80,6 +90,43 @@ public class AuthenticatedQueryResource {
                     .collect(Collectors.toList());
 
             return RestUtils.createCorrectVariant(new ProcessInstanceUserTaskDetailsVariableList(taskDetailsList), headers, Response.Status.OK, conversationIdHeader);
+        } catch (Exception e) {
+            LOGGER.error("Unexpected error during processing {}", e.getMessage(), e);
+            return RestUtils.internalServerError(RestUtils.errorMessage(e), v, conversationIdHeader);
+        }
+    }
+
+    @ApiOperation(value = "Returns information about a specified task instance.", response = ProcessInstanceUserTaskDetailsWithVariables.class)
+    @GET
+    @Path(TASK_GET_URI)
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @KieServerEndpoint(categories = {KieServerEndpoint.EndpointType.DEFAULT, KieServerEndpoint.EndpointType.HISTORY})
+    public Response getTaskById(@Context HttpHeaders headers,
+                                @ApiParam(value = "task id to load task instance", required = true) @PathParam(TASK_INSTANCE_ID) Long taskId) {
+        Variant v = getVariant(headers);
+        Header conversationIdHeader = RestUtils.buildConversationIdHeader("", context, headers);
+
+        try {
+            UserTaskInstanceDesc userTaskInstanceDesc = runtimeDataServiceBase.getTaskById(taskId);
+
+            if (userTaskInstanceDesc == null) {
+                throw new TaskNotFoundException("No task found with id " + taskId);
+            }
+
+            List<org.jbpm.services.api.query.model.QueryParam> attributesQueryParams = new ArrayList<>(3);
+            attributesQueryParams.add(new org.jbpm.services.api.query.model.QueryParam("TABLE", "MODE", Collections.singletonList("HISTORY")));
+            attributesQueryParams.add(new org.jbpm.services.api.query.model.QueryParam(PROCESS_ATTR_INSTANCE_ID, "EQUALS_TO", Collections.singletonList(userTaskInstanceDesc.getProcessInstanceId())));
+            attributesQueryParams.add(new org.jbpm.services.api.query.model.QueryParam(TASK_ATTR_NAME, "EQUALS_TO", Collections.singletonList(userTaskInstanceDesc.getName())));
+
+            AuthQueryUtils authQueryUtils = new AuthQueryUtils(bypassAuthUser, identityProvider, advanceRuntimeDataService, runtimeDataServiceBase);
+
+            List<UserTaskInstanceWithPotOwnerDesc> taskInstanceWithPotOwnerDescs = advanceRuntimeDataService.queryUserTasksByVariables(attributesQueryParams, null, null, null, new QueryContext(0, 0));
+            ProcessInstanceUserTaskDetailsWithVariables taskDetails = taskInstanceWithPotOwnerDescs.stream()
+                    .filter(desc -> userTaskInstanceDesc.getTaskId().equals(desc.getTaskId()))
+                    .map(authQueryUtils.toProcessInstanceUserTaskDetailsWithVariables).findAny().orElseThrow(() -> new TaskNotFoundException("No task found with id " + taskId));
+            return RestUtils.createCorrectVariant(taskDetails, headers, Response.Status.OK, conversationIdHeader);
+        } catch (TaskNotFoundException e) {
+            return notFound(MessageFormat.format("Could not find task instance with id \"{0}\"", taskId), v, conversationIdHeader);
         } catch (Exception e) {
             LOGGER.error("Unexpected error during processing {}", e.getMessage(), e);
             return RestUtils.internalServerError(RestUtils.errorMessage(e), v, conversationIdHeader);
